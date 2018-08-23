c9050acfb402a81ed6b5f143601e8bcb14114462
north <north.tong@163.com>
2018-08-23 23:40:22 +0800

    1.修正了访问路径的说明，因为修改了项目名

--- demo-springboot-web/src/main/java/com/apestore/app/Application.java
+++ demo-springboot-web/src/main/java/com/apestore/app/Application.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import com.apestore.controller.UserController;
 
 //使用main函数运行项目时，访问路径不包含项目名
-//既tomcat下url=http://localhost:8080/springboot-web-demo/user/create
+//既tomcat下url=http://localhost:8080/demo-springboot-web/user/create
 //main函数运行url=http://localhost:8080/user/create
 public class Application extends HttpServlet {
 
