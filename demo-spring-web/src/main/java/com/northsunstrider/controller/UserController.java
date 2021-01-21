abacb8223d1684e25bfda633b19990ff7d13004d
NorthSunstrider <north.tong@163.com>
2021-01-21 11:08:07 +0800

    fix bug A new security advisory was published We found a vulnerable dependency in repositories you have security alert access to.
    Security advisory GHSA-5949-rw7g-wx7w (high severity) affects 2
    repositories:com.fasterxml.jackson.core:jackson-databind (Maven) used in
    2 repositories
    @NorthSunstrider NorthSunstrider/_test
    @NorthSunstrider NorthSunstrider/demo

--- demo-spring-web/src/main/java/com/northsunstrider/controller/UserController.java
+++ demo-spring-web/src/main/java/com/northsunstrider/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.northsunstrider.controller;
 
-import javax.annotation.Resource;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +15,7 @@ import com.northsunstrider.service.UserDataService;
 @RequestMapping("/user")
 public class UserController {
 
-	@Resource
+	@Autowired
 	UserDataService userDataService;
 
 	@RequestMapping(value = "/create", method = RequestMethod.POST)
